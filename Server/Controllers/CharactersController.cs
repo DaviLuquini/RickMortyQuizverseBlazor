ecfc0071e87e88c7a66ee8ee853a1a0a5fc56b64
Davi de Sá Luquini <davi262016@gmail.com>
2024-04-30 00:20:20 -0300

    First commit/ Initial interface

---
+++ Server/Controllers/CharactersController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RickMortyApi.Shared.Models;
+
+namespace RickMortyApi.Server.Controllers
+{
+    [Route("api/characters")]
+    public class CharactersController : Controller
+    {
+       private HttpClient _httpclient;
+
+        public CharactersController()
+        {
+            _httpclient = new HttpClient();
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public async Task<Characters> GetAllCharacters()
+        {
+            try
+            {
+                Characters? characters = null;
+                HttpResponseMessage httpResponseMessage = await 
+                    _httpclient.GetAsync("https://rickandmortyapi.com/api/character");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                string responseBody = await 
+                    httpResponseMessage.Content.ReadAsStringAsync();
+
+                var tempCharacters = JsonConvert.DeserializeObject<Characters>(responseBody);
+
+                if (tempCharacters != null)
+                {
+                    characters = tempCharacters;
+                    return characters;
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+
+            catch (Exception)
+            {
+                throw new ArgumentNullException();
+            }
+
+        }
+    }
+}
---
+++ Shared/Models/Character.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace RickMortyApi.Shared.Models
+{
+        public class Origin
+        {
+            public required string Name { get; set; }
+            public string? Url { get; set; }
+
+        }
+        public class Location
+        {
+            public required string Name { get; set; }
+            public string? Url { get; set; }
+
+        }
+        public class Character
+        {
+            public int Id { get; set; }
+            public string? Name { get; set; }
+            public string? Status { get; set; }
+            public string? Species { get; set; }
+            public string? Type { get; set; }
+            public string? Gender { get; set; }
+            public required Origin Origin { get; set; }
+            public required Location Location { get; set; }
+            public string? Image { get; set; }
+            public IList<string>? Episode { get; set; }
+            public string? Url { get; set; }
+            public DateTime? Created { get; set; }
+
+        }
+}
